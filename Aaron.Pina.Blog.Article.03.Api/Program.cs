622293500615db32cf07e7e9d1b47732d6197096
mucalytic <mucalytic@hotmail.com>
2026-01-08 21:08:30 +0100

    Basic authentication implementation using JWT.

---
+++ Aaron.Pina.Blog.Article.03.Api/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Security.Claims;
+
+using var rsa = RSA.Create(2048);
+var rsaKey = new RsaSecurityKey(rsa);
+var rsaPublicKey = new RsaSecurityKey(rsa.ExportParameters(false));
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        // ValidIssuer = "https://localhost",
+        ValidateAudience = false,
+        // ValidAudience = "https://localhost",
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = rsaPublicKey,
+        // ClockSkew = TimeSpan.FromMinutes(5)
+    };
+});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/login", () =>
+{
+    var now = DateTimeOffset.UtcNow;
+    
+    var claims = new[]
+    {
+        new Claim(JwtRegisteredClaimNames.Sub, Guid.NewGuid().ToString()),
+        new Claim(JwtRegisteredClaimNames.Iat, now.ToUnixTimeSeconds().ToString()),
+        new Claim(JwtRegisteredClaimNames.Name, "Aaron Pina")
+    };
+
+    var tokenDescriptor = new SecurityTokenDescriptor
+    {
+        Issuer = "https://localhost:5001",
+        Audience = "https://localhost:5001",
+        Subject = new ClaimsIdentity(claims),
+        Expires = now.AddMinutes(30).DateTime,
+        SigningCredentials = new SigningCredentials(rsaKey, SecurityAlgorithms.RsaSha256)
+    };
+
+    var handler = new JwtSecurityTokenHandler();
+    var token = handler.CreateToken(tokenDescriptor);
+
+    return Results.Ok(new { Token = handler.WriteToken(token) });
+}).AllowAnonymous();
+
+app.MapGet("/protected", () => "Secret data!")
+   .RequireAuthorization();
+
+app.Run();
